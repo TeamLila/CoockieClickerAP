00776ae4909f922e2077c68f65f83b253662c2f2
TEAM_LILA <realteamlila+git@gmail.com>
2026-01-23 22:22:01 +0100

    Progress save
    what progress? none was made.... flip me

--- apworld/worlds/cookie_clicker/__init__.py
+++ apworld/worlds/cookie_clicker/__init__.py
@@ -1,3 +1,35 @@
-from .world import CookieClickerWorld
+from ..AutoWorld import World
+from BaseClasses import Region
+from .locations import CookieClickerLocation, location_name_to_id
+from .items import item_name_to_id
+from .options import CookieClickerOptions
 
-__all__ = ["CookieClickerWorld"]
+
+#Main Class
+class CookieClickerWorld(World):
+    #what is it internaly called?
+    game = "cookie_clicker"
+    
+    #define the id's
+    location_name_to_id = location_name_to_id
+    item_name_to_id = item_name_to_id
+    
+    #Define options
+    options_dataclass = CookieClickerOptions
+    
+    topology_present = False
+    
+    def create_regions(self):
+        menu_region = Region("Menu", self.player, self.multiworld)
+
+        for name, loc_id in self.location_name_to_id.items():
+            from BaseClasses import Location
+            menu_region.locations.append(Location(self.player, name, loc_id, menu_region))
+
+        self.multiworld.regions.append(menu_region)
+
+# for _ in range(0, 5):
+#     print("------------------------------")
+# print(f"DEBUG ME: {CookieClickerWorld.options_dataclass}")
+# for _ in range(0, 5):
+#     print("------------------------------")
